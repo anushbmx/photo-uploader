57ca6a0b178f49c419eacdb50d16e5fea3e99b71
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-06 20:31:54 +0530

    Add support to delete images
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/card.js
+++ js/card.js
@@ -2,11 +2,13 @@
 
     var photoid = window.location.hash ? location.hash.replace(/^#/, "") : "",
         cardimg = document.getElementById("cardimg"),
-        photo;
+        photo,
+        photos;
 
     // Load photos from localforage
     localforage.getItem("photos").then(function (result) {
         if (result) {
+            photos = result;
             photo = result[photoid];
             if (photo) {
                 console.log("Photo retrieved", photo);
@@ -26,5 +28,31 @@
         console.log("404. Invalid image", photoid);
         alert("Invalid image " + photoid);
     };
+
+    var deletephoto = function () {
+
+        if (confirm("Delete?")) {
+            console.log("Deleting", photoid);
+            var photostorage = navigator.getDeviceStorage("pictures");
+            var trydelete = photostorage.delete(photoid);
+
+            trydelete.onsuccess = function () {
+                console.log("unset", photos[photoid]);
+                delete(photos[photoid]);
+
+                localforage.setItem("photos", photos, function () {
+                    alert("Photo deleted");
+                    window.location.replace("index.html");
+                });
+            };
+
+            trydelete.onerror = function () {
+                alert("Unable to delete photo");
+            };
+        }
+    };
+
+    // Button bindings
+    document.getElementById("deletebutton").addEventListener("click", deletephoto, false);
 
 })();
