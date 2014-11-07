095b93429de3d2f63d7795ebfa059aa7710d7564
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-07 19:21:07 +0530

    Add inline comments to make code self-explanatory
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/app.js
+++ js/app.js
@@ -2,17 +2,29 @@
 
     var takephoto = document.getElementById("takephoto"),
         photogrid = document.getElementById("photogrid"),
+
+        // Create a blank object to store our photos data
         photos = {};
 
-    // Load photos from localforage
+    // Load photos from localforage using `localforage.getItem`.
     localforage.getItem("photos").then(function (result) {
+
+        // Check if any photo exist. If no prior data is present, `result` will be `null`.
         if (result) {
+            // If any data is found, store if in the `photos` object
             photos = result;
         }
         console.log("Photos retrieved", photos);
+
+        // Trigger the function to inject photos in the DOM
         printphotos();
     });
 
+    /**
+     * Inject list of photos to the DOM.
+     *
+     * This function iterates over the `photos` object and calls `appendphoto()` for each item.
+     */
     var printphotos = function () {
         console.log("printing photos", photos);
         for (var key in photos) {
@@ -20,6 +32,9 @@
         }
     };
 
+    /**
+     * Append individual photo elements to the photo grid.
+     */
     var appendphoto = function (photo) {
         if (!document.querySelectorAll(".photo-item").length) {
             photogrid.innerHTML = "";
@@ -28,19 +43,37 @@
         var photowrapper = document.createElement("div");
         console.log("appending", photo);
         photowrapper.setAttribute("class", "column item photo-item");
+
+        // We store necessary information about the current photo item
+        // as `data` attributes on its corresponding DOM element. This
+        // removes the requirement to store the entire `photos` object
+        // in the memory. We can get information about a specific
+        // photo by doing `Element.getAttribute()`.
         photowrapper.setAttribute("data-name", photo.blob.name);
         photowrapper.setAttribute("data-size", photo.blob.size);
         photowrapper.setAttribute("data-type", photo.blob.type);
         photowrapper.setAttribute("data-lastModifiedDate", photo.blob.lastModifiedDate);
+
+        // We use `URL.createObjectURL()` to generate a temporary reference to our photo blob.
         photowrapper.innerHTML = "<img src='" + URL.createObjectURL(photo.blob) + "'>";
+
+        // For each photo item, we do a click binding to go to the card page for that photo.
         photowrapper.addEventListener("click", function () {
+            // We send the photo's `File.name` property as the hash fragment
             window.location.replace("card.html#" + this.getAttribute("data-name"));
         }, false);
+
         photogrid.appendChild(photowrapper);
     };
 
+    /**
+     * Save a photo to our local data store using localforage.
+     *
+     * This function shows the usage of `localforage.setItem`.
+     */
     var savephoto = function (photo) {
         photos[photo.blob.name] = photo;
+
         localforage.setItem("photos", photos, function () {
             console.log("setting photos storage", photos);
             alert("Photo saved.");
--- js/card.js
+++ js/card.js
@@ -5,7 +5,7 @@
         photo,
         photos;
 
-    // Load photos from localforage
+    // Load photos from localforage using `localforage.getItem`.
     localforage.getItem("photos").then(function (result) {
         if (result) {
             photos = result;
@@ -19,26 +19,41 @@
         }
     });
 
+    /**
+     * Set the card image using `URL.createObjectURL`.
+     */
     var printcard = function () {
         console.log("Printing card for photo", photo);
         cardimg.setAttribute("src", URL.createObjectURL(photo.blob));
     };
 
+    /**
+     * Show an error message if photo id is invalid.
+     */
     var show404 = function () {
         console.log("404. Invalid image", photoid);
         alert("Invalid image " + photoid);
     };
 
+    /**
+     * Delete a photo from the Device Storage and from localforage.
+     */
     var deletephoto = function () {
 
+        // Confirm if user wants to delete
         if (confirm("Delete?")) {
+
             console.log("Deleting", photoid);
+
+            // Use device storage to delete a photo.
             var photostorage = navigator.getDeviceStorage("pictures");
             var trydelete = photostorage.delete(photoid);
 
             trydelete.onsuccess = function () {
                 console.log("unset", photos[photoid]);
-                delete(photos[photoid]);
+
+                // If the file on device has been deleted, also update the localforage datastore.
+                delete photos[photoid];
 
                 localforage.setItem("photos", photos, function () {
                     alert("Photo deleted");
