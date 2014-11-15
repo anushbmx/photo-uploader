8671b1e82c40a2682693bb4ef0657bd5e2b378ba
Anush <anush@techstream.org>
2014-11-15 09:38:04 +0530

    Removed all the Code. Its just some comments

--- js/app.js
+++ js/app.js
@@ -1,70 +1,40 @@
-//##(function () {
+(function () {
 
-//##   var takephoto = document.getElementById("takephoto"),
-//##       photogrid = document.getElementById("photogrid"),
 
-        // Create a blank object to store our photos data
-//##       photos = {};
+    // Create a blank object to store our photos data
 
     // Load photos from localforage using `localforage.getItem`.
-//##    localforage.getItem("photos").then(function (result) {
 
-        // Check if any photo exist. If no prior data is present, `result` will be `null`.
-//##        if (result) {
-            // If any data is found, store if in the `photos` object
-//##            photos = result;
-//##        }
-//##        console.log("Photos retrieved", photos);
+    // Check if any photo exist. If no prior data is present, `result` will be `null`.
 
-        // Trigger the function to inject photos in the DOM
-//##        printphotos();
-//##    });
+    // If any data is found, store if in the `photos` object
+
+    // Trigger the function to inject photos in the DOM
+
 
     /**
      * Inject list of photos to the DOM.
      *
      * This function iterates over the `photos` object and calls `appendphoto()` for each item.
      */
-//##    var printphotos = function () {
-//##        console.log("printing photos", photos);
-//##        for (var key in photos) {
-//##            appendphoto(photos[key]);
-//##        }
-//##    };
 
     /**
      * Append individual photo elements to the photo grid.
      */
-//##    var appendphoto = function (photo) {
-//##        if (!document.querySelectorAll(".photo-item").length) {
-//##            photogrid.innerHTML = "";
-//##        }
 
-//##        var photowrapper = document.createElement("div");
-//##        console.log("appending", photo);
-//##        photowrapper.setAttribute("class", "column item photo-item");
 
-        // We store necessary information about the current photo item
-        // as `data` attributes on its corresponding DOM element. This
-        // removes the requirement to store the entire `photos` object
-        // in the memory. We can get information about a specific
-        // photo by doing `Element.getAttribute()`.
-//##        photowrapper.setAttribute("data-name", photo.blob.name);
-//##        photowrapper.setAttribute("data-size", photo.blob.size);
-//##        photowrapper.setAttribute("data-type", photo.blob.type);
-//##        photowrapper.setAttribute("data-lastModifiedDate", photo.blob.lastModifiedDate);
+    // We store necessary information about the current photo item
+    // as `data` attributes on its corresponding DOM element. This
+    // removes the requirement to store the entire `photos` object
+    // in the memory. We can get information about a specific
+    // photo by doing `Element.getAttribute()`.
 
-        // We use `URL.createObjectURL()` to generate a temporary reference to our photo blob.
-//##        photowrapper.innerHTML = "<img src='" + URL.createObjectURL(photo.blob) + "'>";
 
-        // For each photo item, we do a click binding to go to the card page for that photo.
-//##        photowrapper.addEventListener("click", function () {
-            // We send the photo's `File.name` property as the hash fragment
-//##            window.location.replace("card.html#" + this.getAttribute("data-name"));
-//##        }, false);
+    // We use `URL.createObjectURL()` to generate a temporary reference to our photo blob.
 
-//##        photogrid.appendChild(photowrapper);
-//##    };
+    // For each photo item, we do a click binding to go to the card page for that photo.
+
+    // We send the photo's `File.name` property as the hash fragment
 
     /**
      * Save a photo to our local data store using localforage.
@@ -72,37 +42,11 @@
      * This function shows the usage of `localforage.setItem`.
      */
  
-//##    var savephoto = function (photo) {
-//##        photos[photo.blob.name] = photo;
-
-//##        localforage.setItem("photos", photos, function () {
-//##            console.log("setting photos storage", photos);
-//##            alert("Photo saved.");
-//##            appendphoto(photo);
-//##        });
-//##    };
 
     // Trigger a "pick" Web Activity when "Take a Photo" button is clicked
-//##    takephoto.addEventListener("click", function () {
-//##        var photoactivity = new MozActivity({
-//##            name: "pick",
-//##            data: {
-//##                type: "image/jpeg"
-//##            }
-//##        });
 
-        // Handle the activity's success event
-//##        photoactivity.onsuccess = function () {
-//##            var photo = this.result;
-//##            console.log("Got photo", photo);
-//##            savephoto(photo);
-//##        };
+    // Handle the activity's success event
 
-        // Handle the activity's error event
-//##        photoactivity.onerror = function () {
-//##            console.log("error", this.error);
-//##        };
+    // Handle the activity's error event
 
-//##    }, false);
-
-//##})();
+})();
--- js/card.js
+++ js/card.js
@@ -1,115 +1,41 @@
-//##(function () {
-
-//##    var photoid = window.location.hash ? location.hash.replace(/^#/, "") : "",
-//##        cardimg = document.getElementById("cardimg"),
-//##        photo,
-//##        photos;
+(function () {
 
     // Load photos from localforage using `localforage.getItem`.
-//##    localforage.getItem("photos").then(function (result) {
-//##        if (result) {
-//##            photos = result;
-//##            photo = result[photoid];
-//##            if (photo) {
-//##                console.log("Photo retrieved", photo);
-//##                printcard();
-//##            } else {
-//##                show404();
-//##            }
-//##        }
-//##    });
 
     /**
      * Set the card image using `URL.createObjectURL`.
      */
-//##    var printcard = function () {
-//##        console.log("Printing card for photo", photo);
-//##        cardimg.setAttribute("src", URL.createObjectURL(photo.blob));
-//##    };
+
 
     /**
      * Show an error message if photo id is invalid.
      */
-//##    var show404 = function () {
-//##        console.log("404. Invalid image", photoid);
-//##        alert("Invalid image " + photoid);
-//##    };
 
     /**
      * Delete a photo from the Device Storage and from localforage.
      */
-//##    var deletephoto = function () {
-        // Confirm if user wants to delete
-//##        if (confirm("Delete?")) {
 
-//##            console.log("Deleting", photoid);
+    // Confirm if user wants to delete
 
-            // Use device storage to delete a photo.
-//##            var photostorage = navigator.getDeviceStorage("pictures");
-//##            var trydelete = photostorage.delete(photoid);
+    // Use device storage to delete a photo.
 
-//##            trydelete.onsuccess = function () {
-//##                console.log("unset", photos[photoid]);
-
-                // If the file on device has been deleted, also update the localforage datastore.
-//##                delete photos[photoid];
-
-//##                localforage.setItem("photos", photos, function () {
-//##                    alert("Photo deleted");
-//##                    window.location.replace("index.html");
-//##                });
-//##            };
-
-//##            trydelete.onerror = function () {
-//##                alert("Unable to delete photo");
-//##            };
-//##        }
-//##    };
+    // If the file on device has been deleted, also update the localforage datastore.
 
     /**
      * Upload a photo to Imgur.
      */
-//##    var uploadphoto = function() {
-      // Get file object
-//##      var file = photo.blob;
 
-      // Create object for form data
-//##      var fd = new FormData();
+    // Get file object
 
-      // Add file to form data
-//##      fd.append("image", file);
+    // Create object for form data
 
-//##      var xhr = new XMLHttpRequest();
-//##      xhr.open("POST", "https://api.imgur.com/3/upload.json");
-//##      xhr.onload = function() {
-//##        var data = JSON.parse(xhr.responseText).data;
-//##        var imgurURL = data.link;
-//##        photos[photoid].imgurURL = imgurURL;
-//##        localforage.setItem("photos", photos, function () {});
-//##      }
-//##      var clientId = 'Client-ID 1ca3a1cf63cc8bc';
-//##      xhr.setRequestHeader('Authorization', clientId);
-//##      xhr.send(fd);
-//##    };
+    // Add file to form data
 
     /**
      * Share a photo.
      */
-//##    var sharephoto = function() {
-//##      var blob = photos[id].blob;
-//##      new MozActivity({
-//##        name: "share",
-//##        data: {
-//##          type: photos[id].type,
-//##          number: 1,
-//##          blobs: [blob]
-//##        }
-//##      });
-//##    }
+
 
     // Button bindings
-//##    document.querySelector("#deletebutton").addEventListener("click", deletephoto, false);
-//##    document.querySelector("#sharebutton").addEventListener("click", sharephoto, false);
-//##    document.querySelector("#uploadbutton").addEventListener("click", uploadphoto, false);
 
-//##})();
+})();
